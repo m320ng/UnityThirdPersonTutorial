91ecb10fe14de8875d8ec15f08589d3e92bc1a13
m320ng <m320ng@gmail.com>
2014-07-24 19:09:57 +0900

    scripts

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -6,12 +6,14 @@ public class ThirdPersonCamera : MonoBehaviour {
     public float distanceAway = 5f;
     public float distanceUp = 2.5f;
     public float smooth = 10f;
-    public Transform follow;
+    public Transform followXForm;
+	public Vector3 offset = new Vector3(0f, 1.5f, 0f);
 
+	private Vector3 lookDir;
     private Vector3 targetPosition;
     
 	void Start() {
-        follow = GameObject.FindGameObjectWithTag("Player").transform;
+        followXForm = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 	
 	void Update() {
@@ -23,13 +25,20 @@ public class ThirdPersonCamera : MonoBehaviour {
     }
 
     void LateUpdate() {
-        targetPosition = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
+		Vector3 characterOffset = followXForm.position + offset;
+
+		lookDir = characterOffset - transform.position;
+		lookDir.y = 0;
+		lookDir.Normalize();
+		Debug.DrawRay(transform.position, lookDir, Color.green);
+
+        targetPosition = followXForm.position + Vector3.up * distanceUp - followXForm.forward * distanceAway;
         //Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
         //Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
-        Debug.DrawLine(follow.position, targetPosition, Color.magenta);
+        Debug.DrawLine(followXForm.position, targetPosition, Color.magenta);
 
         transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
 
-        transform.LookAt(follow);
+        transform.LookAt(followXForm);
     }
 }
